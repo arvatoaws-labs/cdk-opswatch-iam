7b79c7861183b02fdf24c6b597abb3f698bfd772
Patrick Robinson <patrick.robinson@bertelsmann.de>
2022-06-21 12:01:38 +0200

    Init
    Signed-off-by: Patrick Robinson <patrick.robinson@bertelsmann.de>

--- bin/cdk-opswatch-iam.ts
+++ bin/cdk-opswatch-iam.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkOpswatchIamStack } from '../lib/cdk-opswatch-iam-stack';
 
 const app = new cdk.App();
-new CdkOpswatchIamStack(app, 'CdkOpswatchIamStack', {
+new CdkOpswatchIamStack(app, 'OpswatchIam', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/cdk-opswatch-iam-stack.ts
+++ lib/cdk-opswatch-iam-stack.ts
@@ -1,16 +1,75 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnParameter } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkOpswatchIamStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const param_principal = new CfnParameter(this, 'OpswatchRolePrincipal', {
+      type: 'String',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkOpswatchIamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const opswatch_role = new iam.CfnRole(this, 'OpswatchRole', {
+      roleName: 'OpswatchRole',
+      assumeRolePolicyDocument: {
+        Version: '2012-10-17',
+        Statement: [{
+          Effect: 'Allow',
+          Principal: {
+            AWS: param_principal.valueAsString
+          },
+          Action: 'sts:AssumeRole'
+        }]
+      },
+      policies: [{
+        policyName: 'TagsPolicy',
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [{
+            Sid: 'GenericTagApi',
+            Effect: 'Allow',
+            Action: [
+              'tag:GetResources'
+            ],
+            Resource: '*'
+          }, {
+            Sid: 'CustomTagApis',
+            Effect: 'Allow',
+            Action: [
+              'cloudfront:ListTagsForResource',
+              'route53:ListTagsForResource',
+              'ec2:DescribeTags',
+            ],
+            Resource: '*'
+          }]
+        }
+      }, {
+        policyName: 'DescribePolicy',
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [{
+            Effect: 'Allow',
+            Action: [
+              'acm:DescribeCertificate',
+              'cloudfront:GetDistribution',
+              'route53:GetHealthCheck',
+              'efs:DescribeFileSystems',
+              'ec2:DescribeVolumes',
+              'ec2:DescribeInstances',
+              'ec2:DescribeInstanceCreditSpecifications',
+              'dynamodb:DescribeTable',
+              'sqs:GetQueueAttributes',
+              'rds:DescribeDBInstances',
+              'ec2:DescribeInstanceTypes',
+              'rds:DescribeDBClusters',
+              'rds:DescribeDBClusterParameters',
+              'rds:DescribeDBParameters'
+            ],
+            Resource: '*'
+          }]
+        }
+      }]
+    });
   }
 }
